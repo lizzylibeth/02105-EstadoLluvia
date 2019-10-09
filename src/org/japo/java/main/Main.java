8bb6d886a642bb720b95af57e347d722e7e62314
victo <victo@192.168.1.134>
2019-10-09 23:08:51 +0200

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,40 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Manera 1
+        
+        System.out.print ("Posibilidad de lluvia: ");    //System es una clase,out es un recurso de la clase System. el punto indica el recurso.  
+        System.out.println(true);   
+        
+        //Manera 2
+        
+        //final boolean POSIBILIDADLLUVIA=true;
+        //System.out.print("Posibilidad de lluvia %b ",POSIBILIDADLLUVIA);
+             
+        
+        
+    }//main
+    
+}//class
